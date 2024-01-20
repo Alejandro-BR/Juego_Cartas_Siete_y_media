b9b7ea4393de0ec11138f52225ff566e1b1dcd42
Jmolmel <josemolinamelendez@gmail.com>
2024-01-20 12:39:04 +0100

    Comienzo a trabajar

--- Baraja.java
+++ Baraja.java
@@ -1,9 +1,21 @@
-public class Baraja {
+// @author Alejandro Barrionuevo Rosado
+// @author Jose Molina Melendez
+// Palos
+// Valores o Numeros
+// Numeros de cartas en total seran 40 cartas
+// Una array de la clase carta
+// Un array de 40 cartas
+// Inicializar el numero de cartas
+// Inicializar la baraja (crear la baraja)
+// Extraer la carta
+public class Barajas {
     public static void main(String[] args) {
-        //////// HOLA SOY JOSE
-        //////// HOLA SOY JOSE
-        //////// HOLA SOY JOSE
-        //////// HOLA SOY JOSE
-        String holaSoyJose;
+
+        //////// ATRIBUTOS
+
+        //////// CONSTRUCTORES
+
+        //////// METODOS
+
     }
 }
