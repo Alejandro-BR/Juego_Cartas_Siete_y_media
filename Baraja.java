36c4b5d343e6eb4e181a2d8acfc30d0d03155f1e
Jmolmel <josemolinamelendez@gmail.com>
2024-01-20 12:12:57 +0100

    Prueba para comprobar la union con el main

---
+++ Baraja.java
@@ -0,0 +1,9 @@
+public class Baraja {
+    public static void main(String[] args) {
+        //////// HOLA SOY JOSE
+        //////// HOLA SOY JOSE
+        //////// HOLA SOY JOSE
+        //////// HOLA SOY JOSE
+        String holaSoyJose;
+    }
+}
