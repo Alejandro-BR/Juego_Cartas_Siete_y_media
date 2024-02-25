aeafe72ca76b35d246b73e1860ff65e8d4556d04
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-02-25 20:40:58 +0100

    Iniciador del juego

---
+++ SieteYMedia.bat
@@ -0,0 +1,14 @@
+@REM Alejandro Barrionuevo Rosado
+@REM Jose Molina Melendez
+
+@echo off
+
+echo Iniciando juego...
+
+cd C:\Users\Usuario\Documents\Juego_Cartas_Siete_y_media/src
+
+javac JuegoSieteYMedia.java  
+
+java JuegoSieteYMedia
+
+pause
